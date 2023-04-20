5430643c51fad634e5a3fb5a38b93379f2fb5445
soniabergamini <sounia96@outlook.it>
2023-04-20 02:07:52 +0200

    feat(js): add thumbnails image click

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -14,6 +14,7 @@ for (let i = 0; i < imgUrls.length; i++) {
 
     imgUrls[i] = document.createElement("img");
     imgUrls[i].alt = "slider-img";
+    imgUrls[i].setAttribute("id", `img-${i}`);
     // imgUrls[i].src = `./Assets/img/${currentImg}`;
     imgUrls[i].setAttribute("src", `${currentImg}`);
     console.log(imgUrls[i].src);
@@ -164,3 +165,105 @@ btnBack.addEventListener("click",
 
     }
 );
+
+// CLICK SULLE SINGOLE IMMAGINI THUMBNAILS DI IMAGESBAR
+var imgNunb = 1;
+
+// Funzione All Img
+function cardBarClick() {
+    document.querySelector(".active").classList.add('inactive');
+    document.querySelector('img[class="slide"]').classList.add('dNone');
+    document.querySelector(".active").classList.remove('active');
+}
+
+// Funzione First Img
+function cardClickImg0() {
+    document.getElementById('firstImgBar').classList.add('active');
+    document.getElementById('firstImgBar').classList.remove('inactive');
+    document.getElementById('firstImg').classList.remove('dNone');
+    currentSlide = 0;
+}
+
+// Funzione Imgs
+function cardClickImgs() {
+    document.getElementById(`imgBar-${imgNumb}`).classList.add('active');
+    document.getElementById(`imgBar-${imgNumb}`).classList.remove('inactive');
+    document.getElementById(`img-${imgNumb}`).classList.remove('dNone');
+    currentSlide = imgNumb;
+}
+
+// Funzione Last Img
+function cardClickImg9() {
+    document.getElementById('lastImgBar').classList.add('active');
+    document.getElementById('lastImgBar').classList.remove('inactive');
+    document.getElementById('lastImg').classList.remove('dNone');
+    currentSlide = 9;
+}
+
+// Click 1° Img Slide Bar
+document.getElementById('firstImgBar').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImg0();
+});
+
+// Click 2° Img Slide Bar
+document.getElementById('imgBar-1').addEventListener("click", function () { imgNumb = 1; });
+document.getElementById('imgBar-1').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 3° Img Slide Bar
+document.getElementById('imgBar-2').addEventListener("click", function() { imgNumb = 2; });
+document.getElementById('imgBar-2').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 4° Img Slide Bar
+document.getElementById('imgBar-3').addEventListener("click", function () { imgNumb = 3; });
+document.getElementById('imgBar-3').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 5° Img Slide Bar
+document.getElementById('imgBar-4').addEventListener("click", function () { imgNumb = 4; });
+document.getElementById('imgBar-4').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 6° Img Slide Bar
+document.getElementById('imgBar-5').addEventListener("click", function () { imgNumb = 5; });
+document.getElementById('imgBar-5').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 7° Img Slide Bar
+document.getElementById('imgBar-6').addEventListener("click", function () { imgNumb = 6; });
+document.getElementById('imgBar-6').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 8° Img Slide Bar
+document.getElementById('imgBar-7').addEventListener("click", function () { imgNumb = 7; });
+document.getElementById('imgBar-7').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 9° Img Slide Bar
+document.getElementById('imgBar-8').addEventListener("click", function () { imgNumb = 8; });
+document.getElementById('imgBar-8').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImgs();
+});
+
+// Click 10° Img Slide Bar
+document.getElementById('lastImgBar').addEventListener("click", () => {
+    cardBarClick();
+    cardClickImg9();
+});
